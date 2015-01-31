785f025d60b70ddc2950ce268070b008ecf1a026
Saul M <saulmm2@gmail.com>
2015-01-31 14:29:20 +0100

    Created MovieView and OnItemClickListener to the recycler adapter

---
+++ HackVG/app/src/main/java/com/hackvg/android/view/MovieView.java
@@ -0,0 +1,9 @@
+package com.hackvg.android.view;
+
+/**
+ * Root class for view classes
+ */
+public interface MovieView {
+
+    public android.content.Context getContext();
+}
---
+++ HackVG/app/src/main/java/com/hackvg/android/view/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.hackvg.android.view;
+
+import android.view.View;
+
+public interface OnItemClickListener {
+
+    public void onClick(View v, int position);
+
+}
