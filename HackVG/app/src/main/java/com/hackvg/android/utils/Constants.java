43d85b8e630c4e8098a5f0bde47fd13f6d93975a
Saul M <saulmm2@gmail.com>
2015-01-31 11:33:21 +0100

    Created constants class with apt & host

---
+++ HackVG/app/src/main/java/com/hackvg/android/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.hackvg.android.utils;
+
+/**
+ * Created by saulmm on 31/01/15.
+ */
+public class Constants {
+
+    public static final String API_KEY = "87a901020f496977f9d6d508c5d186ec";
+    public static final String MOVIE_DB_HOST = "http://api.themoviedb.org/3/";
+}
