d51056669068cf51eb086571d0af0dcc220b5a24
Saul M <saulmm2@gmail.com>
2015-01-31 11:00:15 +0100

    Added basic interfaces

---
+++ HackVG/app/src/main/java/com/hackvg/android/model/Entity.java
@@ -0,0 +1,7 @@
+package com.hackvg.android.model;
+
+/**
+ * Created by saulmm on 31/01/15.
+ */
+public interface Entity {
+}
---
+++ HackVG/app/src/main/java/com/hackvg/android/view/View.java
@@ -0,0 +1,7 @@
+package com.hackvg.android.view;
+
+/**
+ * Created by saulmm on 31/01/15.
+ */
+public interface View {
+}
