f4785f77f8b3b5ec7601823d90dd2936c15bf73c
Saul M <saulmm2@gmail.com>
2015-02-19 13:06:37 +0100

    Added missing presenter

---
+++ HackVG/app/src/main/java/com/hackvg/android/mvp/presenters/MoviesPresenter.java
@@ -0,0 +1,58 @@
+package com.hackvg.android.mvp.presenters;
+
+import com.hackvg.android.mvp.views.MoviesView;
+import com.hackvg.common.utils.BusProvider;
+import com.hackvg.common.utils.Constants;
+import com.hackvg.domain.ConfigurationUsecaseController;
+import com.hackvg.domain.GetMoviesUsecase;
+import com.hackvg.domain.GetMoviesUsecaseController;
+import com.hackvg.domain.Usecase;
+import com.hackvg.model.entities.PopularMoviesApiResponse;
+import com.hackvg.model.rest.RestMovieSource;
+import com.squareup.otto.Subscribe;
+
+
+public class MoviesPresenter extends Presenter {
+
+    private final MoviesView mMoviesView;
+
+    public MoviesPresenter(MoviesView mMoviesView) {
+
+        this.mMoviesView = mMoviesView;
+    }
+
+    @Subscribe
+    public void onPopularMoviesReceived(PopularMoviesApiResponse popularMovies) {
+
+        mMoviesView.hideLoading();
+        mMoviesView.showMovies(popularMovies.getResults());
+    }
+
+    @Subscribe
+    public void onConfigurationFinished (String baseImageUrl) {
+
+        Constants.BASIC_STATIC_URL = baseImageUrl;
+
+        Usecase getPopularShows = new GetMoviesUsecaseController(
+            GetMoviesUsecase.TV_MOVIES, RestMovieSource.getInstance(), BusProvider.getUIBusInstance());
+
+        getPopularShows.execute();
+    }
+
+    @Override
+    public void start() {
+
+        BusProvider.getUIBusInstance().register(this);
+
+        mMoviesView.showLoading();
+
+        Usecase configureUsecase = new ConfigurationUsecaseController();
+        configureUsecase.execute();
+    }
+
+    @Override
+    public void stop() {
+
+        BusProvider.getUIBusInstance().unregister(this);
+    }
+}
