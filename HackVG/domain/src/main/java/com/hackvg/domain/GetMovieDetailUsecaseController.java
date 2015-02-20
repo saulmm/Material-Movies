bd0a234eb0470c980874531d66ddd840cbfe611e
Saul M <saulmm2@gmail.com>
2015-02-21 00:45:22 +0100

    Refactored GetMovieDetailUsecaseController

--- HackVG/domain/src/main/java/com/hackvg/domain/GetMovieDetailUsecaseController.java
+++ HackVG/domain/src/main/java/com/hackvg/domain/GetMovieDetailUsecaseController.java
@@ -3,22 +3,40 @@ package com.hackvg.domain;
 import com.hackvg.common.utils.BusProvider;
 import com.hackvg.model.MediaDataSource;
 import com.hackvg.model.entities.MovieDetailResponse;
-import com.hackvg.model.rest.RestMovieSource;
+import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 
 /**
- * Created by saulmm on 31/01/15.
+ * This class is an implementation of {@link com.hackvg.domain.GetMovieDetailUsecase}
  */
-public class GetMovieDetailUsecaseController implements GetMovieDetail {
+public class GetMovieDetailUsecaseController implements GetMovieDetailUsecase {
 
-    private final String movieID;
-    private final MediaDataSource movieDataSource;
+    private final MediaDataSource mMovieDataSource;
+    private final String mMovieId;
+    private final Bus mUiBus;
 
-    public GetMovieDetailUsecaseController(String movieID) {
+    /**
+     * Constructor of the class.
+     *
+     * @param movieId The id of the movie to retrieve the details
+     * @param uiBus The bus to communicate the domain module and the app module
+     * @param dataSource The data source to retrieve the details of the movie
+     */
+    public GetMovieDetailUsecaseController(String movieId,
+        Bus uiBus, MediaDataSource dataSource) {
 
-        this.movieID = movieID;
+        if (movieId == null)
+            throw new IllegalArgumentException("Movie Id cannot be null");
 
-        movieDataSource = RestMovieSource.getInstance();
+        if (uiBus == null)
+            throw new IllegalArgumentException("UiBus cannot be null");
+
+        if (dataSource == null)
+            throw  new IllegalArgumentException("MediaData source cannot be null");
+
+        mMovieId = movieId;
+        mUiBus = uiBus;
+        mMovieDataSource = dataSource;
 
         BusProvider.getRestBusInstance().register(this);
     }
@@ -26,20 +44,27 @@ public class GetMovieDetailUsecaseController implements GetMovieDetail {
     @Override
     public void requestMovieDetail(String id) {
 
-        movieDataSource.getDetailMovie(id);
+        mMovieDataSource.getDetailMovie(id);
     }
 
     @Subscribe
     @Override
-    public void sendResponseToPresenter(MovieDetailResponse response) {
+    public void onMovieDetailResponse(MovieDetailResponse response) {
 
-        BusProvider.getUIBusInstance().post(response);
         BusProvider.getRestBusInstance().unregister(this);
+        sendDetailMovieToPresenter(response);
     }
+
+    @Override
+    public void sendDetailMovieToPresenter(MovieDetailResponse response) {
+
+        mUiBus.post(response);
+    }
+
 
     @Override
     public void execute() {
 
-        requestMovieDetail(movieID);
+        requestMovieDetail(mMovieId);
     }
 }
