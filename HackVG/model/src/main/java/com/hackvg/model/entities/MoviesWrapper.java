0baffc9f96150bb2f673dc0571bd02e6d7fdef9c
Saul M <saulmm2@gmail.com>
2015-03-04 17:04:25 +0100

    Added movie wrapper

---
+++ HackVG/model/src/main/java/com/hackvg/model/entities/MoviesWrapper.java
@@ -0,0 +1,54 @@
+
+package com.hackvg.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class MoviesWrapper implements Serializable {
+    private Number page;
+
+    private List<TvMovie> results;
+
+    private Number total_pages;
+    private Number total_results;
+
+    public MoviesWrapper(List<TvMovie> results) {
+
+        this.results = results;
+    }
+
+    public Number getPage() {
+
+        return this.page;
+    }
+
+    public void setPage(Number page) {
+
+        this.page = page;
+    }
+
+    public List<TvMovie> getResults() {
+
+        return results;
+    }
+
+    public Number getTotal_pages() {
+
+        return this.total_pages;
+    }
+
+    public void setTotal_pages(Number total_pages) {
+
+        this.total_pages = total_pages;
+    }
+
+    public Number getTotal_results() {
+
+        return this.total_results;
+    }
+
+    public void setTotal_results(Number total_results) {
+
+        this.total_results = total_results;
+    }
+}
