86d5902c4eb8d7e044b81d43bd7e34dbc5957d58
Saul M <saulmm2@gmail.com>
2015-03-10 13:45:16 +0100

    Added custom listeners

---
+++ HackVG/app/src/main/java/com/hackvg/android/views/custom_listeners/CustomAnimatorListener.java
@@ -0,0 +1,27 @@
+package com.hackvg.android.views.custom_listeners;
+
+
+import android.animation.Animator;
+
+public class CustomAnimatorListener implements Animator.AnimatorListener {
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
---
+++ HackVG/app/src/main/java/com/hackvg/android/views/custom_listeners/CustomTransitionListener.java
@@ -0,0 +1,32 @@
+package com.hackvg.android.views.custom_listeners;
+
+import android.transition.Transition;
+
+
+public class CustomTransitionListener implements Transition.TransitionListener {
+
+    @Override
+    public void onTransitionStart(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionEnd(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionCancel(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionPause(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionResume(Transition transition) {
+
+    }
+}
