adaf9aeaf56d61fa3acf7ce0c149ef0f4e590ac9
Saul M <saulmm2@gmail.com>
2015-06-18 19:19:21 +0200

    Licensed all the code

--- HackVG/app/src/androidTest/java/com.hackvg.android.test/views.activities/MoviesActivityTest.java
+++ HackVG/app/src/androidTest/java/com.hackvg.android.test/views.activities/MoviesActivityTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 Saúl Molinero.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hackvg.android.test.views.activity;
 
 import android.support.test.espresso.Espresso;
@@ -13,9 +28,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
-/**
- * Created by saulmm on 17/02/15.
- */
 public class MoviesActivityTest extends ActivityInstrumentationTestCase2<MoviesActivity> {
 
     private MoviesActivity mMoviesActivity;
